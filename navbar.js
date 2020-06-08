93f73d068b798bf533be242fb7493b3a0069baef
Abdillahzakkie <zakariyyaopeyemi@gmail.com>
2020-06-08 10:33:21 +0100

    Made some changes

---
+++ navbar.js
@@ -0,0 +1,3 @@
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+navToggle.addEventListener("click", () => links.classList.toggle("show-links"));
